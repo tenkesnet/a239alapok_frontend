725f7b81e4cc140d6a17d862b1e7fcf3ccb947d3
Bárdonicsek Róbert <rbardo@gmail.com>
2025-11-22 13:40:28 +0100

    Funkcionális- és osztály- komponensek

--- src/App.js
+++ src/App.js
@@ -1,42 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import SpeakerProfile from './SpeakerProfile';
 
-const person = { name: 'John', age: 30, city: 'New York' };
-
-console.log({ ...person, name: 'Johnny' });
-
-const speakers = {
-  id: 1,
-  name: "Juliet Runolf",
-  jobTitle: "Director, Marketing",
-  company: "Abernatny Group",
-  address: {
-    street: "Okaland Dtuse",
-    city: "Greenland",
-    state: "Houston",
-    country: "USA",
-  }
-}
 
 
 function App() {
-  const { name, jobTitle, company, address } = speakers;
-  const { street, city, state, country } = address;
+  const name='Pallas PlatformXXXXXXXXX';
   return (
     <div className="App">
-      <header className="App-header">
-    <div> <h2>Name: {name}</h2>
-        <h4>Position: {jobTitle}</h4>
-        <h4>Company: {company}</h4>
-        <h4>Street: {street}</h4>
-        <h4>City: {city}</h4>
-        <h4>State: {state}</h4>
-        <h4>Country: {country}</h4>
+      <div>
+        <h1>Welcome to {name}</h1>
+        <p>This is a sample React application.</p>
+        <SpeakerProfile />
+        <SpeakerProfile />
+        
       </div>
-      </header>
-      
     </div>
   );
 }
+
 
 export default App;
