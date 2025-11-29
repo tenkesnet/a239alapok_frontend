c66c05a6f78d8f85fcca7cb2efd26ce61030cce1
Bárdonicsek Róbert <rbardo@gmail.com>
2025-11-29 09:21:40 +0100

    Eseménykavalkád és példacunami

--- src/SearchSpeaker.js
+++ src/SearchSpeaker.js
@@ -2,23 +2,32 @@ import React, { useState } from "react";
 const SearchSpeaker = () => {
     const [searchText, setSearchText] = useState('cégtár');
     const [cegNev, setCegNev] = useState('Pallas Platform');
-    const peldaClick= () => {
+    const peldaClick = () => {
         setSearchText('React Kft');
     }
-    const tesztClick= () => {
+    const tesztClick = (e) => {
         console.log(searchText);
+    }
+    function handleChange(e) {
+        console.log(e);
+        e.target.className = 'alert';
+
     }
     return (
         <div>
-            <label htmlFor="search" onClick={e => {setSearchText('Label clicked');setCegNev('Labeled cegnev');}}>Search speaker:
+            <label htmlFor="search" onClick={e => {
+                setSearchText('Label clicked');
+                setCegNev('Labeled cegnev');
+            }
+            } on>Search speaker:
             </label>
-            <input id="search" type="text" onChange={e =>
-                setSearchText(e.target.value)} onBlur={e => setSearchText('Pokol')}/>
+            <input id="search" type="text" onClick={handleChange} onChange={e =>
+                setSearchText(e.target.value)} onBlur={e => { setSearchText('Pokol'); e.target.className = ''; }} />
             <p>
                 Searching for <strong>{searchText}</strong> in {cegNev}
             </p>
-            <button onClick={e=>peldaClick()}>Példa gomb</button>
-            <button onClick={tesztClick}>Teszt gomb</button>
+            <button onClick={e => peldaClick()}>Példa gomb</button>
+            <button onClick={handleChange}>Teszt gomb</button>
         </div>
     );
 }
