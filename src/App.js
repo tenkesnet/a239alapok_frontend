a1a73654e4cd5f465daec5b6f5a22baaf5fcb564
Bárdonicsek Róbert <rbardo@gmail.com>
2025-12-06 09:08:58 +0100

    state gyakorlatok asyncronitásra

--- src/App.js
+++ src/App.js
@@ -4,11 +4,28 @@ import UserList from './components/v2/UserList';
 
 
 class App extends React.Component {
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      azEletErtelem: 42,
+      incrementValue: props.incrementValue || 1
+    };
+  }
   render() {
     return (
       <div className="App">
-        <UserList />
+        <h1>Üdv a Felhasználói alkalmazásban!</h1>
+        <h2>Az élet értelme: {this.state.azEletErtelem}</h2>
+        <button onClick={() => {
+          this.setState({ azEletErtelem: this.state.azEletErtelem + this.state.incrementValue });
+          console.log('Az élet értelme most már:', this.state.azEletErtelem);
+          setTimeout(() => {
+            console.log('Időzített élet értelme:', this.state.azEletErtelem);
+          }, 1);
+        }
+        }>Növeld az élet értelmét!</button>
+        <pre>{JSON.stringify(this.state, null, 2)}</pre>
+        {/*<UserList />*/}
       </div>
     );
 
