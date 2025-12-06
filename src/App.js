448bdd854c0d1d629f58b5305599711933714b8b
Bárdonicsek Róbert <rbardo@gmail.com>
2025-12-06 08:37:13 +0100

    Funkcionális app konvertálása osztály alpura

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
+import React from 'react';
 import './App.css';
 import UserList from './components/v2/UserList';
 
 
-function App() {
-  
-  return (
-    <div className="App">
-      <UserList />
-    </div>
-  );
-}
+class App extends React.Component {
 
+  render() {
+    return (
+      <div className="App">
+        <UserList />
+      </div>
+    );
+
+  }
+}
 
 export default App;
